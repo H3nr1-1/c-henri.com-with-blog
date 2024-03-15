8b02715e83d34b21c8090bfe6dc33d9ca9906c4a
Chris <henri@c-henri.com>
2024-03-15 09:13:54 -0500

    small changes
    made small changes to speed up the server response time.  Eliminated the mp4 video from all pages other than the blog_home.php

--- connect-db.php
+++ connect-db.php
@@ -1,7 +1,11 @@
 <?php 
-$servername = "mysql:host=localhost;dbname=yrrujvxy_henri_blog";
-$username = "yrrujvxy_chris";
-$password = "defendtheden81";
+// $servername = "mysql:host=localhost;dbname=yrrujvxy_henri_blog";
+// $username = "yrrujvxy_chris";
+// $password = "defendtheden81";
+
+$servername = "mysql:host=localhost;dbname=henri_blog";
+$username = "root";
+$password = "root";
 
 try {
     $db = new PDO($servername, $username, $password);
